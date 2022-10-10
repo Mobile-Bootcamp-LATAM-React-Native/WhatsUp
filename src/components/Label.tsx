4e22c47c6d321548cf98fdc1e1d826533c27efd5
Israel Aguilar <isra.neri2@gmail.com>
2022-10-10 18:50:48 -0400

    Added label component

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -1,19 +1,75 @@
 import { StyleSheet, Text, TextProps } from 'react-native'
 import React from 'react'
+import { useContext } from 'react';
+import { ColorContext } from '../shared/ColorContext';
+
+type LabelType = 'h0' | 'h1' | 'h2' | 'h3' | 'h4' | 'p1' | 'p2' 
 
 type LabelProps = {
+  text: string;
+  type?: LabelType;
 }
 
-// TODO: h0, h1, h2, h3, h4, p1, p2
-// Typografy
-const Label = (props: LabelProps & TextProps) => {
+const Label = ({
+  text,
+  type = 'p1',
+  style
+}: LabelProps & TextProps) => {
+  const styles = useStyles();
+
   return (
-    <Text {...props}>
-      {props.children}
+    <Text style= {[styles[type], style]} >
+      {text}
     </Text>
   )
 }
 
 export default Label
 
-const styles = StyleSheet.create({})
+const useStyles = () => {
+  const {
+    primaryColorText
+  } = useContext(ColorContext);
+
+  return StyleSheet.create({
+    h0: {
+      fontSize: 40,
+      lineHeight: 41,
+      color: primaryColorText,
+    },
+    h1: {
+      fontSize: 32,
+      lineHeight: 41,
+      fontWeight: 'bold',
+      color: primaryColorText,
+    },
+    h2: {
+      fontSize: 24,
+      lineHeight: 33,
+      fontWeight: '600',
+      color: primaryColorText,
+    },
+    h3: {
+      fontSize: 16,
+      lineHeight: 20,
+      fontWeight: 'bold',
+      color: primaryColorText,
+    },
+    h4: {
+      fontSize: 14,
+      lineHeight: 19,
+      fontWeight: 'bold',
+      color: primaryColorText,
+    },
+    p1: {
+      fontSize: 13,
+      lineHeight: 20,
+      color: primaryColorText,
+    },
+    p2: {
+      fontSize: 14,
+      lineHeight: 19,
+      color: primaryColorText,
+    }
+  });
+}

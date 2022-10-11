273de610488e7afbdb55ff810c6eabfbef3f2f24
YawarValeriano <yawarvs@gmail.com>
2022-10-11 08:12:04 -0400

    Added base format with examples in Home Page

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,15 +1,53 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Image, ImageBackground, ImageBackgroundProps, StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
 
-// TODO
-const Avatar = () => {
-  return (
-    <View>
-      <Text>Avatar</Text>
-    </View>
-  )
+type AvatarSize = 'xs' | 'sm' | 'md' | 'lg' | 'xl' |'2xl'
+
+type AvatarProps = {
+  src: string,
+  size?: AvatarSize
+}
+
+const Avatar = ({
+  src,
+  size = 'md'
+}: AvatarProps & ImageBackgroundProps) => {
+  const styles = getStyles(size)
+  const [error, setError] = useState(false)
+  return (<ImageBackground style = {styles.container} source = {error? require("../assets/default_avatar.png") : {uri: src} } onError={() => setError(true)}/>)  
 }
 
 export default Avatar
 
-const styles = StyleSheet.create({})
+const resolveSize = (size: AvatarSize) => {
+  switch (size) {
+    case 'xs':
+      return 38
+    case 'sm':
+      return 55
+    case 'md':
+      return 76
+    case 'lg':
+      return 99
+    case 'xl':
+      return 128
+    case '2xl':
+      return 160
+    default:
+      return 0
+  }
+}
+
+const getStyles = (size: AvatarSize) => {
+  const resolvedSize = resolveSize(size)
+  return StyleSheet.create({
+    container: {
+      margin: 5,
+      overflow: 'hidden',
+      borderRadius: resolvedSize,
+      backgroundColor: 'gray',
+      width: resolvedSize,
+      height: resolvedSize
+    }
+  })
+}

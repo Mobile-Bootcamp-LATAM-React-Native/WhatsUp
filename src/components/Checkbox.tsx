5aed5ebd3bcd106251bde9692625165b9ebfe172
Rodrigo-Schar <Rodrigo.Schar@jala-foundation.org>
2022-10-11 14:32:37 -0400

    added checkbox component

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,15 +1,41 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { useContext } from 'react'
+import { ColorContext } from '../shared/ColorContext';
+//import { Checkbox } from 'native-base';
+//import Checkbox from './src/components/Checkbox';
 
-// TODO
-const Checkbox = () => {
+type CheckboxProps = {
+  text: string;
+  value: string;
+}
+
+const CheckBox  = ({text, value}: CheckboxProps) => {
+  const styles = useStyles();
   return (
     <View>
-      <Text>Checkbox</Text>
+      <Checkbox value={value} style={styles.checkbox}>{text}</Checkbox>
     </View>
   )
 }
 
-export default Checkbox
+export default CheckBox
 
-const styles = StyleSheet.create({})
+const useStyles = () => {
+  const {
+    primaryColor,
+    primaryButtonText,
+    margin
+  } = useContext(ColorContext)
+
+  return StyleSheet.create({
+    checkbox: {
+      backgroundColor: primaryColor,
+      color: primaryButtonText,
+      margin: margin,
+    }
+  });
+}
+
+/*
+npm install react-native-base --save
+npm install native-base react-native-svg@12.1.1 react-native-safe-area-context@3.3.2*/

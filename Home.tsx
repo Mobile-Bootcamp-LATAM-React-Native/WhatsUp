5180d13b5e3dfbc4ce3b3e4027d68c170467669d
adCBueno <adrianacc.bueno@gmail.com>
2022-10-11 10:19:01 -0400

    loading correction color

--- Home.tsx
+++ Home.tsx
@@ -1,18 +1,33 @@
-import { SafeAreaView, StyleSheet, Text, useColorScheme } from 'react-native'
-import React from 'react'
+import { ActivityIndicator, SafeAreaView, StyleSheet, Text, useColorScheme } from 'react-native'
+import React, { useState } from 'react'
 
 import { useMyTheme } from './src/hooks'
 import Button from './src/components/Button';
 import ColorContext from './src/shared/ColorContext';
+import Loading from './src/components/Loading';
 
 const Home = () => {
+  const [loading, setLoading] = useState(false);
   const theme = useMyTheme();
+
+  const startLoading = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  };
 
   return (
     <ColorContext value={theme}>
       <SafeAreaView style={[styles.main, { backgroundColor: theme.backgroundColor }]}>
-        <Button text="Cancel" type="secundary" style={{ width: 120 }} />
-        <Button text="Yes"/>
+        {loading ? (
+          <Loading />
+        ) : (
+          <>
+            <Button text="Cancel" type="secundary" style={{ width: 120 }} />
+            <Button text="Yes" />
+          </>
+        )}
       </SafeAreaView>
     </ColorContext>
   )

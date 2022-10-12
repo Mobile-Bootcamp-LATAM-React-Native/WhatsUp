28404e1f77220a17224c5bba72adad0b30e207a6
gaguayovelez <37461997+gaguayovelez@users.noreply.github.com>
2022-10-12 15:35:18 -0400

    Add stack navigation

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
 
 import {AppRegistry} from 'react-native';
 import Home from './Home';
-import SignIn from './src/screens/SignIn';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => SignIn);
+AppRegistry.registerComponent(appName, () => Home);
---
+++ src/navigation/AppNavigation.tsx
@@ -0,0 +1,21 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import { Messenger, SignIn, Storyboard } from '@/features';
+import { RootStackParamList } from './RootStack';
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+const AppNavigation = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Storyboard" component={Storyboard} />
+        <Stack.Screen name="SignIn" component={SignIn} />
+        <Stack.Screen name="Messenger" component={Messenger} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default AppNavigation

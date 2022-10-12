609ba4bca7b2d74d527bb8c76517e83b7de137a2
Israel Aguilar <isra.neri2@gmail.com>
2022-10-12 14:22:34 -0400

    Added Label storyboard component

---
+++ src/components/Label.stories.tsx
@@ -0,0 +1,14 @@
+import { storiesOf } from '@storybook/react-native';
+import Label from './Label';
+import { View } from 'react-native';
+
+storiesOf('Label', module)
+  .addDecorator((story) => <View>{story()}</View>)
+  .add('default', () => <Label>New Label</Label>)
+  .add('h0', () => <Label type='h0'>New Label</Label>)
+  .add('h1', () => <Label type='h1'>New Label</Label>)
+  .add('h2', () => <Label type='h2'>New Label</Label>)
+  .add('h3', () => <Label type='h3'>New Label</Label>)
+  .add('h4', () => <Label type='h4'>New Label</Label>)
+  .add('p1', () => <Label type='p1'>New Label</Label>)
+  .add('p2', () => <Label type='p2'>New Label</Label>)
